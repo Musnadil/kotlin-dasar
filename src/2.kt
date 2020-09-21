67dd80331add2154e89d9e5c387aa7645c638817
Musnadil <firdaus.musnadil@gmail.com>
2020-09-21 20:48:25 +0700

    pertemuan 2

---
+++ src/2.kt
@@ -0,0 +1,33 @@
+fun main () {
+    var age: Byte = 30
+    var height: Int = 130
+    var distance: Short = 2000
+    var balance: Long = 100000000L
+
+    var firstName: String = "MUSNADIL"
+    var lastname: String = "FIRDAUS"
+    var fullname: String = "Musnadil Firdaus"
+    var namadawa: String = firstName+" "+lastname
+    var address: String = """
+        disini
+        disana
+        kepo
+    """.trimIndent()
+    val jeneng : String = "kulo"
+
+    println(age)
+    println(height)
+    println(distance)
+    println(balance)
+
+    println(jeneng)
+    println(firstName)
+    println(lastname)
+    println(namadawa)
+    println(fullname)
+    println(address)
+}
+
+
+
+
