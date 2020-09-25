6d0e85f0ed68f727544313ebc8362f45fe6e44dc
Musnadil <firdaus.musnadil@gmail.com>
2020-09-25 20:04:46 +0700

    pertemuan 4

---
+++ src/4.kt
@@ -0,0 +1,23 @@
+fun main(){
+    val range = 0..100
+    val range2 = 100 downTo 0 step 2
+
+    for (i in 1 until 10) {
+        println("data ke-$i = $i,")
+    }
+    for (i in 1..10) {
+        println("data ke-$i = $i,")
+    }
+
+
+    println(range.count())
+    println(range.contains(9))
+    println(range.contains(12))
+    println(range.first)
+    println(range.last)
+    println(range.step)
+    println(range.step)
+    println(range.contains(45))
+    println(range2)
+
+}
